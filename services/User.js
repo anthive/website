50cd5151ffd6913069122f69a4b1a94e4caee365
Klimonov <22pov22@gmail.com>
2020-01-18 17:15:11 +0300

    draft user service

--- services/User.js
+++ services/User.js
@@ -1,90 +1,121 @@
-export { getUserdata, photoUrl, langUrl,antUrl ,hiveUrl,scoreString,timeAgo}
+// export { getUserdata, photoUrl, langUrl,antUrl ,hiveUrl,scoreString,timeAgo}
+export default User;
 
 import axios from "axios";
 
 const baseURL = "https://anthive.io/";
 const githubURL = "https://api.github.com/";
 
-function getUserdata(username) {
-  if (username === '' || username === undefined) return
-
-  var ghAxios = axios.create({
-    baseURL: githubURL,
-    timeout: 30000
-  });
-
-  return ghAxios.get('users/' + username)
-    .then((res) => {
-      var user = res.data;
-
-      return {
-        name: (user.name != null) ? user.name : username,
-        avatar: this.photoUrl(username, 250),
-        company: user.company,
-        location: user.location,
-        blog: (user.blog != '') ? user.blog : null
-      };
-    }).catch(handleError);
-}
-
-function photoUrl(username, size) {
-  if (username.startsWith('sample-')) {
-    return langUrl(username.substring(7));
+class User {
+  constructor() {
+    this.Username = ''
+    this.Lang = ''
+    this.Version = ''
+    this.Games = ''
+    this.Wealth = ''
+    this.Wg = ''
   }
-  return "https://github.com/" + username + ".png?size=" + size;
-}
 
-function langUrl(lang) {
-  return baseURL + "skins/lang/" + lang + ".png";
-}
+  async getUser(id) {
+    console.log('fill Profile User func')
+    this.Id = id
+    let filled = await Fire.firestore()
+      .collection(usersFBCollection)
+      .doc(id)
+      .get()
+      .then(doc => {
+        const data = doc.data()
+        if (data) return true
+        else return false
+      })
+      .catch(error => console.error(error.message))
+    if (filled) await this.init(id)
+    else await this.save(id)
 
-function antUrl(skin, big = false) {
-  if (big) {
-    return baseURL + "skins/client/" + skin + "/antBig.png";
+    return filled
   }
-  return baseURL + "skins/client/" + skin + "/ant.png";
-}
 
-function hiveUrl(skin, big = false) {
-  if (big) {
-    return baseURL + "skins/client/" + skin + "/hiveBig.png";
+  static getUserdata() {
+    if (this.Username === '' || this.Username === undefined) return
+
+    var ghAxios = axios.create({
+      baseURL: githubURL,
+      timeout: 30000
+    });
+
+    return ghAxios.get('users/' + this.Username)
+      .then((res) => {
+        var user = res.data;
+        console.log('--------', user)
+        return {
+          name: (user.name != null) ? user.name : this.Username,
+          avatar: this.photoUrl(250),
+          company: user.company,
+          location: user.location,
+          blog: (user.blog != '') ? user.blog : null
+        };
+      }).catch(handleError);
   }
-  return baseURL + "skins/client/" + skin + "/hive.png";
-}
 
-function scoreString(value) {
-  var suffixes = ["", "K", "M", "B", "t"];
-  var suffixNum = Math.floor((("" + value).length - 1) / 3);
-  //console.log(value, suffixNum);
-  var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(2));
-  return shortValue + suffixes[suffixNum];
-}
-
-function timeAgo(epochStamp) {
-  const diff = Math.round(new Date()) - epochStamp;
-  const periods = {
-    month: 30 * 24 * 60 * 60 * 1000,
-    week: 7 * 24 * 60 * 60 * 1000,
-    day: 24 * 60 * 60 * 1000,
-    hour: 60 * 60 * 1000,
-    minute: 60 * 1000,
-    second: 1000,
-  };
-  if (diff > periods.month) {
-    // it was at least a month ago
-    return Math.floor(diff / periods.month) + "m ago";
-  } else if (diff > periods.week) {
-    return Math.floor(diff / periods.week) + "w ago";
-  } else if (diff > periods.day) {
-    return Math.floor(diff / periods.day) + "d ago";
-  } else if (diff > periods.hour) {
-    return Math.floor(diff / periods.hour) + "h ago";
-  } else if (diff > periods.minute) {
-    return Math.floor(diff / periods.minute) + "m ago";
-  } else if (diff > periods.second) {
-    return Math.floor(diff / periods.second) + "s ago";
+  static photoUrl(size = 70) {
+    if (this.Username.startsWith('sample-')) {
+      return langUrl(this.Username.substring(7));
+    }
+    return "https://github.com/" + this.Username + ".png?size=" + size;
   }
-  return "Just now";
+
+  static langUrl(lang = this.Lang) {
+    return baseURL + "skins/lang/" + lang + ".png";
+  }
+
+  static antUrl(skin, big = false) {
+    if (big) {
+      return baseURL + "skins/client/" + skin + "/antBig.png";
+    }
+    return baseURL + "skins/client/" + skin + "/ant.png";
+  }
+
+  static hiveUrl(skin, big = false) {
+    if (big) {
+      return baseURL + "skins/client/" + skin + "/hiveBig.png";
+    }
+    return baseURL + "skins/client/" + skin + "/hive.png";
+  }
+
+  static scoreString() {
+    var suffixes = ["", "K", "M", "B", "t"];
+    var suffixNum = Math.floor((("" + this.Wealth).length - 1) / 3);
+    //console.log(value, suffixNum);
+    var shortValue = parseFloat((suffixNum != 0 ? (this.Wealth / Math.pow(1000, suffixNum)) : this.Wealth).toPrecision(2));
+    return shortValue + suffixes[suffixNum];
+  }
+
+  static timeAgo(epochStamp) {
+    const diff = Math.round(new Date()) - epochStamp;
+    const periods = {
+      month: 30 * 24 * 60 * 60 * 1000,
+      week: 7 * 24 * 60 * 60 * 1000,
+      day: 24 * 60 * 60 * 1000,
+      hour: 60 * 60 * 1000,
+      minute: 60 * 1000,
+      second: 1000,
+    };
+    if (diff > periods.month) {
+      // it was at least a month ago
+      return Math.floor(diff / periods.month) + "m ago";
+    } else if (diff > periods.week) {
+      return Math.floor(diff / periods.week) + "w ago";
+    } else if (diff > periods.day) {
+      return Math.floor(diff / periods.day) + "d ago";
+    } else if (diff > periods.hour) {
+      return Math.floor(diff / periods.hour) + "h ago";
+    } else if (diff > periods.minute) {
+      return Math.floor(diff / periods.minute) + "m ago";
+    } else if (diff > periods.second) {
+      return Math.floor(diff / periods.second) + "s ago";
+    }
+    return "Just now";
+  }
 }
 
 function handleError(error) {
