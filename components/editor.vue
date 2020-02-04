92e13edcc69c16ede0af6ca99c60f871181ab52b
staszigzag <stas.zigzag@gmail.com>
2020-02-04 17:13:37 +0300

    event

--- components/editor.vue
+++ components/editor.vue
@@ -1,14 +1,14 @@
 <template lang="pug" >
-  v-card {{editors}}
+  v-card(height="500") 
     v-toolbar(color='grey' dark flat)
       v-toolbar-title Your Sandbox
       template(v-slot:extension)
         v-tabs(v-model='tab' show-arrows)
           v-tabs-slider(color='yellow')
-          v-tab(v-for="(lang, key) in langs" :key='key') {{ lang }}
+          v-tab(@change="onChangeTab(key)" v-for="(lang, key) in langs" :key='key') {{ lang }}
     v-tabs-items(v-model='tab')
       v-tab-item(eager v-for="(lang, key) in langs" :key='key')
-        v-card(height='300' flat='')
+        v-card(height='400' flat='')
           v-card-text
             div.editor(:id='key')
 </template>
@@ -20,8 +20,6 @@ import 'ace-builds/src-min-noconflict/theme-monokai'
 import 'ace-builds/src-min-noconflict/ext-language_tools'
 import 'ace-builds/src-min-noconflict/mode-javascript'
 import 'ace-builds/src-min-noconflict/snippets/javascript'
-import 'ace-builds/src-min-noconflict/worker-javascript'
-
 import 'ace-builds/src-min-noconflict/mode-golang'
 import 'ace-builds/src-min-noconflict/snippets/golang'
 import 'ace-builds/src-min-noconflict/mode-c_cpp'
@@ -47,58 +45,59 @@ export default {
       php: 'https://raw.githubusercontent.com/anthive/php/master/run.php',
       c_cpp: 'https://raw.githubusercontent.com/anthive/cpp/master/bot.cpp'
     },
+    currentLangTab: null,
     tab: null,
     editors: {},
     ed: null,
   }),
+  props: {
+    valueCode: {
+      type: Object,
+      default: () => {}
+    }
+  },
   mounted() {
     this.initEditors()
-    // this.ed = ace.edit("JavaScript", {
-    //     theme: "ace/theme/monokai",
-    //     mode: "ace/mode/javascript",
-    //     highlightGutterLine: false,
-    //     autoScrollEditorIntoView: true,
-    //     enableBasicAutocompletion: true,
-    //     enableSnippets: true,
-    //     enableLiveAutocompletion: true
-    // });
-    // this.ed2 = ace.edit("editor2", {
-    //     theme: "ace/theme/monokai",
-    //     mode: "ace/mode/javascript",
-    //     autoScrollEditorIntoView: true,
-    //     enableBasicAutocompletion: true,
-    //     enableSnippets: true,
-    //     enableLiveAutocompletion: true
-    // });
-  //   axios.get('https://raw.githubusercontent.com/anthive/js/master/run.js')
-  //   .then((response) => {
-  //   // handle success
-  //   this.ed.setValue(response.data)
-  //   // this.ed2.setValue(response.data)
-  //   console.log(response.data, this.ed);
-  // })
-    // var editor = ace.edit('editor')
-    // editor.setTheme('ace/theme/monokai')
-    // editor.session.setMode('ace/mode/javascript')
-    // // editor.session.setMode('ace/mode/golang')
-    // console.log(ace)
+  },
+  beforeDestroy() {
+    for (const lang in this.editors) {
+      this.editors[lang].destroy()
+    }
   },
   methods: {
+    onChangeTab(lang) {
+      console.log(lang)
+      this.currentLangTab = lang
+      this.$emit('update:valueCode', {
+        lang: lang,
+        value: `${this.editors[lang].getValue()}`
+      })
+    },
     initEditors() {
       for (const lang in this.botTemplates) {
         if (this.botTemplates.hasOwnProperty(lang)) {
-          console.log(23423, lang);
           const ed = ace.edit(lang, {
             theme: "ace/theme/monokai",
             mode: `ace/mode/${lang}`,
+            fontSize: 14,
             autoScrollEditorIntoView: true,
             enableBasicAutocompletion: true,
             enableSnippets: true,
             enableLiveAutocompletion: true
           })
+          ed.on('change',(z) => {
+            if(this.currentLangTab === lang) {
+              this.$emit('update:valueCode', {
+                lang: lang,
+                value: `${this.editors[lang].getValue()}`
+              })
+            }
+          })
           axios.get(this.botTemplates[lang])
-            .then(res => ed.setValue(res.data))
-            .then(() => this.editors[land] = ed)
+            .then(res => {
+              ed.setValue(res.data)
+              this.editors[lang] = ed
+            })
             .catch(er => console.log(er))
         }
       }    
@@ -127,62 +126,3 @@ export default {
 }
 </style>
 
-//     watch:{
-//         value:function (val) {
-//             if(this.contentBackup !== val){
-//                 this.editor.session.setValue(val,1);
-//                 this.contentBackup = val;
-//             }
-//         },
-//         theme:function (newTheme) {
-//             this.editor.setTheme('ace/theme/'+newTheme);
-//         },
-//         lang:function (newLang) {
-//             this.editor.getSession().setMode(typeof newLang === 'string' ? ( 'ace/mode/' + newLang ) : newLang);
-//         },
-//         options:function(newOption){
-//             this.editor.setOptions(newOption);
-//         },
-//         height:function(){
-//             this.$nextTick(function(){
-//                 this.editor.resize()
-//             })
-//         },
-//         width:function(){
-//             this.$nextTick(function(){
-//                 this.editor.resize()
-//             })
-//         }
-//     },
-//     beforeDestroy: function() {
-//         this.editor.destroy();
-//         this.editor.container.remove();
-//     },
-//     mounted: function () {
-//         var vm = this;
-//         var lang = this.lang||'text';
-//         var theme = this.theme||'chrome';
-
-//         require('brace/ext/emmet');
-
-//         var editor = vm.editor = ace.edit(this.$el);
-//         editor.$blockScrolling = Infinity;
-
-//         this.$emit('init',editor);
-        
-//         //editor.setOption("enableEmmet", true);
-//         editor.getSession().setMode(typeof lang === 'string' ? ( 'ace/mode/' + lang ) : lang);
-//         editor.setTheme('ace/theme/'+theme);
-//         if(this.value)
-//             editor.setValue(this.value,1);
-//         this.contentBackup = this.value;
-
-//         editor.on('change',function () {
-//             var content = editor.getValue();
-//             vm.$emit('input',content);
-//             vm.contentBackup = content;
-//         });
-//         if(vm.options)
-//             editor.setOptions(vm.options);
-//     }
-// }
--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -3,12 +3,12 @@
 		v-parallax(src='/img/home_bg.png', height='300')
 		v-row.mx-2
 			v-col(cols="12")
-				v-card.white.pa-3.elevation-6.rules__card
+				v-card(min-height="700").white.pa-3.elevation-6.rules__card
 					v-card-title {{ "sandbox" }}
 					v-row
-						v-col.conte(cols="6")
+						v-col.conte(cols="12" md="6")
 							//- v-card-text.white-space {{ $t('rulesText') }}
-							editor
+							editor(:valueCode.sync="valueCode")
 								//- code.python def openFile(path): file = open(path, "r") content = file.read() file.close() return content
 						v-col(cols="6")
 							#player
@@ -18,12 +18,18 @@ import editor from '@/components/editor.vue'
 
 export default {
   data: () => ({
+    valueCode: {},
     isPlaying: true
   }),
   mounted() {
     const dataUrl = 'https://storage.googleapis.com/anthive-dev-games/Kj5A2SydOFlqgLIu35Br.zip'
     // eslint-disable-next-line
 	new AnthivePlayer('#player', dataUrl)
+  },
+  watch: {
+    valueCode(e) {
+      console.log(e.value)
+    }
   },
   components: {
     editor
