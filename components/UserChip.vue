e4ea3a6c3bbfcc1e78534d7d509d882c9ac435d6
EgoriusRus <egorius26rus@gmail.com>
2020-03-12 13:11:49 +0300

    game page redesign

--- components/UserChip.vue
+++ components/UserChip.vue
@@ -1,72 +1,65 @@
 <template lang="pug">
-	.user-info-full.elevation-1
-		v-card.user-info-full__card.pa-2(@click="handlerClickChip" color="#D8D8D8")
+	.user-info-full.elevation-1.d-flex
+		v-card.user-info-full__card.pa-2(:to="{ path: '/user/?username=' + us.Username }" color="#262626" tile)
 			v-row.px-3
+				p.user-info-full__number {{number}}
 				v-col.pa-0(cols="7")
 					.d-flex
-						v-avatar(size="60")
+						v-avatar.py-4.user-info-full__avatar(size="60")
 							v-img(:src="us.photoUrl(100)" class="grey lighten-4")
 						v-avatar.user-info-full__ant-img.ml-n4(size="32")
-							v-img(:src="us.antUrl(true)")
-						.d-flex.flex-wrap
-							v-card-text.pa-1.mx-auto.text-left.font-weight-bold.subtitle-1 {{ us.Username }}
-							.d-flex.mt-n3.mb-n1
-								v-card-text.pa-1.caption
-									span.text-left.user-info-full__meta-item-title {{ $t('infoEarned') }} {{ player.Wealth }}
-								v-col.pa-1
-									v-avatar(size="30" color="white")
-										span.caption.font-weight-bold v.{{ us.Version }}
-								v-col.pa-1
-									v-avatar(size="30")
-										v-img(:src="us.langUrl(us.Lang)")
+							v-img(:src="us.antUrl(true)") 
+							
 		v-expand-transition
-			v-card.transition-fast-in-fast-out(v-if="showBotInfo" :to="{ path: '/user/?username=' + us.Username }" color="#EEEEEE")
-				.px-5.pb-6.pt-4.subtitle-1.font-weight-medium {{ $t('infoBotInfo') }}
-					.d-flex
-						v-col.col-6.pa-0
+			v-card.user-info-full__statistic(color="#1a1a1a" tile :class="{'opened': isOpened}")
+				.user-info-full__statistic-value.pa-4.pb-1.mx-auto.text-left.font-weight-bold.subtitle-1 {{ us.Username }}
+				.pa-4.pt-1.tb-1.subtitle-1.font-weight-medium
+					.d-flex.user-info-full__statistic-container
+						v-col.col-4.pa-0
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoSpan') }}
-								span Y{{ player.Y }}, X{{ player.X }}
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoSpan') }}
+								span.user-info-full__statistic-value Y{{ player.Y }}, X{{ player.X }}
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoTicks') }}
-								span {{ player.Stats.Age }}
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoTicks') }}
+								span.user-info-full__statistic-value {{ player.Stats.Age }}
+						v-col.col-4.pa-0
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoAnts') }}
-								span {{ player.Stats.Ants }}
-						v-col.col-6.pa-0
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoAnts') }}
+								span.user-info-full__statistic-value {{ player.Stats.Ants }}
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoHiveSize') }}
-								span {{ player.Stats.Hive }}
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoHiveSize') }}
+								span.user-info-full__statistic-value {{ player.Stats.Hive }}
+						v-col.col-4.pa-0
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoErrors') }}
-								span {{ player.Stats.Errors }}
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoErrors') }}
+								span.user-info-full__statistic-value {{ player.Stats.Errors }}
 							v-card-text.pa-0.body-2
-								span.mr-1 {{ $t('infoEarned') }}
-								span {{ player.Wealth }}
+								span.user-info-full__statistic-key.mr-1 {{ $t('infoEarned') }}
+								span.user-info-full__statistic-value {{ player.Wealth }}
+					.user-info-full__statistic-info-badges
+						v-col.pa-1
+							v-avatar(size="30" color="white")
+								span.caption.font-weight-bold v.{{ us.Version }}
+						v-col.pa-1
+							v-avatar(size="30")
+								v-img(:src="us.langUrl(us.Lang)")
 </template>
 
 <script>
 import { User } from '@/services/User'
 
 export default {
-  name: 'usesrInfo',
+  name: 'usersInfo',
   props: {
     player: Object,
-    locale: String
+    locale: String,
+    number: Number,
+    isOpened: Boolean
   },
   data: () => ({
     us: null,
     showBotInfo: false
   }),
-  methods: {
-    handlerClickChip() {
-      if (this.locale) {
-        this.$router.push('/user/?username=' + this.us.Username)
-        return
-      }
-      this.showBotInfo = !this.showBotInfo
-    }
-  },
   created() {
     this.us = new User()
     this.us.initUser(this.player)
@@ -77,14 +70,62 @@ export default {
 <style>
 .user-info-full {
   display: inline-block;
-  border-radius: 38px;
-  overflow: hidden;
-  width: 279px;
+  border-bottom: 2px solid #404040;
+}
+
+@media screen and (max-width: 997px) {
+  .user-info-full {
+    flex-direction: column;
+  }
+}
+
+.user-info-full__number {
+  color: #555555;
+}
+
+.user-info-full__avatar {
+  box-sizing: content-box;
+}
+
+.user-info-full__statistic.opened {
+  display: block;
+  width: 460px;
+}
+
+@media screen and (max-width: 997px) {
+  .user-info-full__statistic.opened {
+    width: 297px;
+  }
+}
+
+.user-info-full__statistic {
+  display: none;
+  width: 0;
+}
+
+@media screen and (max-width: 997px) {
+  .user-info-full__statistic-container {
+    flex-direction: column;
+  }
+}
+
+.user-info-full__statistic-key {
+  color: #75715e;
+}
+
+.user-info-full__statistic-value {
+  color: #cccccc;
+}
+.user-info-full__statistic-info-badges {
+  position: absolute;
+  top: 10px;
+  right: 5px;
+  display: flex;
 }
 
 .user-info-full__ant-img {
   background-color: white;
-  border: 3px solid #d8d8d8;
+  border: 3px solid #262626;
   box-sizing: border-box;
 }
 
--- pages/game.vue
+++ pages/game.vue
@@ -1,51 +1,76 @@
 <template lang="pug">
   section
-    v-row.flex-nowrap.ma-8()
-      .d-flex.flex-column.mr-5.mt-n2
-        template(v-for="(player, index) in players")
-          .d-flex
-            span.d-flex.flex-column.justify-center.headline.mr-4.font-weight-bold {{ index + 1 }}
-            userChip.my-2(
-              :player="player"
-            )
-      v-col.pa-0.player__section(cols="auto" style="overflow: auto;")
-        .player__wrap(@click="playPause()" @mouseover="showActionsState = true" @mouseleave="showActionsState = false" ref="playerWrap")
-        div(id="player")
-          h2.px-2.white--text(class="loading") {{status}}
-        v-slide-y-transition
-          .end-game-layout(v-show="isGameEnd")
-            .layout-buttons
-              v-btn.layout-button.mb-6(color="#00BF70" dark width="320" height="72" block :href="`https://profile.anthive.io/new-game/?rematch=${this.gameId}`") Request rematch
-              v-btn.layout-button(color="#333333" dark width="320" height="72" block @click="replay()") Replay
-                v-icon.ml-1(dark) autorenew
-              .social-share
-                p Share the game
-                .d-flex.justify-center
-                  v-tooltip(bottom)
-                    template(v-slot:activator="{ on }")
-                      v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`https://www.linkedin.com/shareArticle?mini=true&url=${currentEncudeUrl}&title=Anthive.io game`")
-                        v-icon mdi-linkedin
-                    span Share on Linkedin
-                  v-tooltip(bottom)
-                    template(v-slot:activator="{ on }")
-                      v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`https://www.facebook.com/sharer/sharer.php?u=${currentEncudeUrl}`")
-                        v-icon mdi-facebook
-                    span Share on Facebook
-                  v-tooltip(bottom)
-                    template(v-slot:activator="{ on }")
-                      v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`http://twitter.com/share?url=/&text=Anthive.io Game - ${currentEncudeUrl}`")
-                        v-icon mdi-twitter
-                    span Share on Twitter
-                  v-tooltip(bottom)
-                    template(v-slot:activator="{ on }")
-                      v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`mailto:?subject=Anthive.io&body=Game - ${currentEncudeUrl}`")
-                        v-icon mdi-email
-                    span Send by Email
-                  v-tooltip(bottom)
-                    template(v-slot:activator="{ on }")
-                      v-btn.mx-3(fab dark color="#333333" v-on="on" @click="copyToClipboard()") 
-                        v-icon mdi-file-document-box-multiple
-                    span Copy URL
+    v-row.flex-nowrap.ma-8
+      .players__wrap(v-show="gameLoaded") 
+        .players__list-toggle(@click="togglePlayerList()")
+           v-icon(class="players__list-toggle-icon mdi" color="white" size="40"  :class="[showPlayerList ? 'mdi-chevron-left'  : 'mdi-chevron-right']")
+        .d-flex.flex-column.mr-5.players__list
+          template(v-for="(player, index) in players")
+            .d-flex
+              userChip(
+                :player="player"
+                :number="index + 1"
+                :isOpened="showPlayerList"
+              )
+      .player-zone__wrap(:class="{'collapsed': showPlayerList}")
+        v-col.pa-0.player__section(cols="auto" style="overflow: auto; height:500px; widht: 50%" )
+          .player__wrap(@click="playPause()" @mouseover="showActionsState = true" @mouseleave="showActionsState = false" ref="playerWrap")
+          div(id="player")
+            h2.px-2.white--text(class="loading") {{status}}
+          v-slide-y-transition
+            .end-game-layout(v-show="isGameEnd")
+              .layout-buttons
+                v-btn.layout-button.mb-6(color="#00BF70" dark width="320" height="72" block :href="`https://profile.anthive.io/new-game/?rematch=${this.gameId}`") Request rematch
+                v-btn.layout-button(color="#333333" dark width="320" height="72" block @click="replay()") Replay
+                  v-icon.ml-1(dark) autorenew
+                .social-share
+                  p Share the game
+                  .d-flex.justify-center
+                    v-tooltip(bottom)
+                      template(v-slot:activator="{ on }")
+                        v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`https://www.linkedin.com/shareArticle?mini=true&url=${currentEncudeUrl}&title=Anthive.io game`")
+                          v-icon mdi-linkedin
+                      span Share on Linkedin
+                    v-tooltip(bottom)
+                      template(v-slot:activator="{ on }")
+                        v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`https://www.facebook.com/sharer/sharer.php?u=${currentEncudeUrl}`")
+                          v-icon mdi-facebook
+                      span Share on Facebook
+                    v-tooltip(bottom)
+                      template(v-slot:activator="{ on }")
+                        v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`http://twitter.com/share?url=/&text=Anthive.io Game - ${currentEncudeUrl}`")
+                          v-icon mdi-twitter
+                      span Share on Twitter
+                    v-tooltip(bottom)
+                      template(v-slot:activator="{ on }")
+                        v-btn.mx-3(fab dark color="#333333" v-on="on" target="_blank" :href="`mailto:?subject=Anthive.io&body=Game - ${currentEncudeUrl}`")
+                          v-icon mdi-email
+                      span Send by Email
+                    v-tooltip(bottom)
+                      template(v-slot:activator="{ on }")
+                        v-btn.mx-3(fab dark color="#333333" v-on="on" @click="copyToClipboard()") 
+                          v-icon mdi-file-document-box-multiple
+                      span Copy URL
+        .log-panel__wrap(class="mt-3" v-show="gameLoaded")
+          v-tabs(dark hide-slider) 
+            v-tab(ripple='' dark)
+              | Requests
+            v-tab(ripple='' dark)
+              | Response
+            v-tab(ripple='' dark)
+              | Logs
+            v-tab-item
+              v-card(flat='' dark)
+                v-card-text
+                  pre.log-panel__text Requests go here...
+            v-tab-item
+              v-card(flat='' dark)
+                v-card-text 
+                  pre.log-panel__text Response go here...
+            v-tab-item
+              v-card(flat='' dark)
+                v-card-text
+                  pre.log-panel__text Logs go here...
 </template>
 
 <script>
@@ -56,7 +81,9 @@ export default {
     status: 'Loading...',
     theme: 1,
     players: [],
-    isGameEnd: false
+    isGameEnd: false,
+    showPlayerList: false,
+    gameLoaded: false
   }),
   components: {
     userChip
@@ -72,6 +99,7 @@ export default {
       // eslint-disable-next-line
       player.on(AnthivePlayer.event.READY, () => {
         this.players = player.framer.playerList.sort(this.compare)
+        this.gameLoaded = true
       })
       // eslint-disable-next-line
       player.on(AnthivePlayer.event.END, () => {
@@ -95,6 +123,9 @@ export default {
     showActions() {
       this.showActionsState = !this.showActionsState
     },
+    togglePlayerList() {
+      this.showPlayerList = !this.showPlayerList
+    },
     async copyToClipboard() {
       try {
         await this.$copyText(this.currentUrl)
@@ -117,10 +148,68 @@ export default {
 }
 </script>
 <style>
+.v-content__wrap {
+  background: #080808;
+}
+
+.player-zone__wrap {
+  width: 90%;
+}
+
+.player-zone__wrap.collapsed {
+  width: 60%;
+}
+
 #player {
   background-repeat: repeat;
   background-color: #388e3c;
 }
+
+.players__wrap {
+  position: relative;
+}
+
+.players__list {
+  height: 987px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: relative;
+  background-color: #1a1a1a;
+}
+
+.players__list-toggle {
+  position: absolute;
+  width: 40px;
+  height: 60px;
+  right: -20px;
+  z-index: 1;
+  background: rgba(25, 25, 25, 0.8);
+  border-radius: 0px 8px 8px 0px;
+  cursor: pointer;
+}
+
+.players__list-toggle-icon {
+  font-size: 40px;
+  margin: 10px;
+  margin-left: 0px;
+  color: white;
+}
+
+.players__list::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #1a1a1a;
+}
+
+.players__list::-webkit-scrollbar {
+  width: 5px;
+  background-color: #f5f5f5;
+}
+
+.players__list::-webkit-scrollbar-thumb {
+  background-color: #403f3f;
+  border: none;
+}
+
 .game__vs-separator {
   position: relative;
   top: -80px;
@@ -138,7 +227,6 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
   height: 100%;
 }
 .end-game-layout {
@@ -169,5 +257,59 @@ export default {
   color: white;
   text-align: center;
   font-size: 16px;
+}
+
+.log-panel__wrap {
+  height: 476px;
+  background-color: #262626;
+}
+
+.log-panel__wrap > .v-tabs {
+  height: 100%;
+}
+
+.log-panel__wrap .v-tabs-items {
+  height: 90%;
+  background-color: #191919;
+  overflow: auto;
+}
+.log-panel__wrap .v-card__text {
+  background-color: #191919;
+}
+
+.log-panel__wrap .v-slide-group__wrapper {
+  background: #343434;
+  align-items: center;
+}
+
+.log-panel__wrap .v-tab {
+  border: 2px solid #474747;
+  margin: 0 10px;
+  text-transform: none;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  color: #595959 !important;
+}
+
+.log-panel__wrap .v-tab--active {
+  background: #595959;
+  border: 1px solid #404040;
+  color: #ffffff !important;
+}
+
+.log-panel__wrap .v-slide-group__content {
+  height: 35px;
+  margin: 0 10px;
+}
+
+.log-panel__text {
+  font-family: Consolas;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;
 }
 </style>
