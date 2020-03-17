9984651f1a5a00e0af570e4902c5581435d3ef59
Klimonov <22pov22@gmail.com>
2020-03-17 18:59:58 +0300

    remove rule items to the Rules page

--- pages/rules.vue
+++ pages/rules.vue
@@ -2,7 +2,7 @@
   section#inspire.rules
     sidebar
     v-card.card
-      rule-content
+      rule-content(:rule="currentRule")
     
 </template>
 
@@ -15,7 +15,44 @@ export default {
     sidebar,
     ruleContent
   },
-  data: () => ({})
+  data: () => ({
+    currentRule: null,
+    rules: {
+      'rule-1': {
+        chapter: 'Chapter 1',
+        title: 'Rule 1',
+        text: '<kbd>Lorem ipsum dolor sit amet, consectetur</kbd>'
+      },
+      'rule-2': {
+        chapter: 'Chapter 1',
+        title: 'Rule 2',
+        text: '<code>&lt;Lorem ipsum dolor sit amet, consectetur&gt;</code>'
+      },
+      'rule-3': { chapter: 'Chapter 1', title: 'Rule 3', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-4': { chapter: 'Chapter 2', title: 'Rule 4', text: 'Lorem ipsum dolor sit amet, consectetur' },
+      'rule-5': { chapter: 'Chapter 2', title: 'Rule 5', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-6': { chapter: 'Chapter 2', title: 'Rule 6', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-7': { chapter: 'Chapter 3', title: 'Rule 7', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-8': { chapter: 'Chapter 3', title: 'Rule 8', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-9': { chapter: 'Chapter 3', title: 'Rule 9', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-10': { chapter: 'Chapter 4', title: 'Rule 10', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-11': { chapter: 'Chapter 4', title: 'Rule 11', text: 'Lorem ipsum dolor sit amet, consectetur ' },
+      'rule-12': { chapter: 'Chapter 4', title: 'Rule 12', text: 'Lorem ipsum dolor sit amet, consectetur ' }
+    }
+  }),
+  created() {
+    this.currentRule = this.rules['rule-1']
+  },
+  computed: {
+    ruleName() {
+      return this.$route.params.rule
+    }
+  },
+  watch: {
+    ruleName() {
+      this.currentRule = this.rules[this.ruleName]
+    }
+  }
 }
 </script>
 
@@ -29,7 +66,7 @@ export default {
 
 .card {
   width: 100%;
-  max-width: 800px;
+  max-width: 950px;
   margin: 20px auto;
   height: 100%;
   padding: 40px;
