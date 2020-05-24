d0c6a5396d2336c4e3442cc34cc19ca405ebea60
EgoriusRus <egorius26rus@gmail.com>
2020-05-24 17:34:55 +0300

    fixed fonts

--- pages/rules.vue
+++ pages/rules.vue
@@ -1,12 +1,23 @@
-<template lang="pug">
-	section
-		v-parallax(src="/img/pre-footer.jpg", align='', height='300')
-		v-row.justify-center
-			v-col(cols="11", md="8")
-				v-card.white.text-xs-left.pa-3.elevation-6.rules__card(flat='', tile='')
-					v-card-title.font-weight-bold.mb-5 {{ $t('rules') }}
-					v-card-text.white-space {{ $t('rulesText') }}
+<template>
+  <section>
+    <v-parallax src="/img/pre-footer.jpg" align="" height="300"></v-parallax>
+    <v-row class="justify-center">
+      <v-col cols="11" md="8">
+        <v-card
+          class="white text-xs-left pa-3 elevation-6 rules__card"
+          flat=""
+          tile=""
+        >
+          <v-card-title class="primary--text font-weight-bold mb-5">{{
+            $t("rules")
+          }}</v-card-title>
+          <v-card-text class="primary--text white-space">{{ $t("rulesText") }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
 </template>
+
 
 <style>
 .rules__card {
