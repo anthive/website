c2713f07a765b54a5c8398579eb226cbcf400ab0
EgoriusRus <egorius26rus@gmail.com>
2020-06-05 09:50:57 +0300

    pp and tos was moved from profile to website

---
+++ pages/pp.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <v-parallax src="/img/pre-footer.jpg" align="" height="300"></v-parallax>
+    <v-row class="justify-center">
+      <v-col cols="11" md="8">
+        <v-card class="white text-xs-left pa-3 elevation-6 rules__card" flat="" tile="">
+          <v-card-title class="primary--text font-weight-bold mb-5">Privacy Policy</v-card-title>
+          <v-card-text class="primary--text white-space">
+           </v-card-text
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<style>
+.policy__card {
+  margin-top: -178px;
+}
+
+.white-space {
+  white-space: pre-line;
+}
+</style>
