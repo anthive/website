da47ccef3c0e83d616fc973c15b6653e2df51d31
EgoriusRus <egorius26rus@gmail.com>
2020-12-21 11:45:14 +0300

    switch ants and hives on horizontal bot card

--- components/AntHiveBotHorizontal.vue
+++ components/AntHiveBotHorizontal.vue
@@ -17,7 +17,7 @@
     <p class="name"><strong>{{ getStringTruncated(bot.displayName, 10) }}</strong> v {{ bot.v }}</p>
     <div class="statistics">
       <div class="mr-12">
-        <p>{{ $t("game.size") }}: <strong>{{ bot.stats.ants }}/{{ bot.stats.hive }}</strong></p>
+        <p>{{ $t("game.size") }}: <strong>{{ bot.stats.hive }}/{{ bot.stats.ants }}</strong></p>
         <p>{{ $t("game.score") }}: <strong>{{ getNumberTruncatedToThousand(bot.stats.score) }}</strong></p>
       </div>
       <div>
