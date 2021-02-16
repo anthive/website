cb9ea0cc2d4aa0e9cae7d313beb5f31c6404b7fe
EgoriusRus <egorius26rus@gmail.com>
2021-02-16 10:32:28 +0300

    update truncate logic, styles

--- mixins/truncate.js
+++ mixins/truncate.js
@@ -4,8 +4,21 @@ const Truncate = {
       const truncatedString = string.length > size ? `${string.substr(0, size - 1)}...` : string
       return truncatedString
     },
-    getNumberTruncatedToThousand(number) {
-      const truncatedNumber = number > 1000 ? `${Math.floor(number / 1000)}K` : number
+    getNumberTruncated(number) {
+      let truncatedNumber
+      switch (true) {
+        case number >= 1000 && number < 1000000:
+          truncatedNumber = `${Math.floor(number / 1000)}K`
+          break
+
+        case number >= 1000000:
+          truncatedNumber = `${Math.floor(number / 1000000)}M`
+          break
+
+        default:
+          truncatedNumber = number
+          break
+      }
       return truncatedNumber
     }
   }
